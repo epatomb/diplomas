aa1f137ff6df3efe03eae1d19a2b9e109ecabdb4
Patricio Betancourt <epatomb@gmail.com>
2020-06-30 09:20:52 -0500

    Final
    Genera diplomas final

--- php/diplomaBasica.php
+++ php/diplomaBasica.php
@@ -15,17 +15,17 @@ $pdf = new FPDF('L','pt','A4');
 $pdf->SetTopMargin(20); $pdf->SetLeftMargin(20); $pdf->SetRightMargin(20); 
 
 $pdf->AddPage(); 
-$pdf->Image('../php/lib/fpdf/PlantillasDiplomas/1.jpg' , 0 ,0, 825, 600);
+$pdf->Image('../php/lib/fpdf/PlantillasDiplomas/3.jpg' , 0 ,0, 825, 600);
 $pdf->SetFont('Arial','B',34); 
-$pdf->SetXY(320,220); 
-$pdf->Cell(120,175,utf8_decode($nombre)); 
+$pdf->SetXY(240,300);  
+$pdf->Cell(0,0,utf8_decode($nombre)); 
 $pdf->SetFont('Arial','B',28);
-$pdf->SetXY(260,320); 
-$pdf->Cell(120,175,utf8_decode($curso)); 
-$pdf->SetFont('Arial','B',20);
-$pdf->SetXY(500,530); 
-$lugarf="Quito, {$fecha}";
-$pdf->Cell(0,0,utf8_decode($lugarf));
+$pdf->SetXY(160,390);  
+$pdf->Cell(0,0,utf8_decode($curso)); 
+//$pdf->SetFont('Arial','B',20);
+//$pdf->SetXY(500,530); 
+//$lugarf="Quito, {$fecha}";
+//$pdf->Cell(0,0,utf8_decode($lugarf));
 $pdf->Output('D','diplomaPrimariaCLP.pdf');
 //header("Location:../index.html#seccion-primaria");
 ?>
